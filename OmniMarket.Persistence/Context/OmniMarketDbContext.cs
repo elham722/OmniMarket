21f40775399a5a7eae3d28f0265312e59f482f88
Elham <elham.heydari722@gmail.com>
2025-02-24 16:52:34 +0330

    add infrastructure and persistensce layer with dbcontext

---
+++ OmniMarket.Persistence/Context/OmniMarketDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OmniMarket.Domain.Entities;
+
+namespace OmniMarket.Persistence.Context
+{
+    public class OmniMarketDbContext:DbContext
+    {
+        public OmniMarketDbContext(DbContextOptions<OmniMarketDbContext> options):base(options)
+        {
+            
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
