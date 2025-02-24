b11dbd510b791e9c6cdafbf04c7d5ea8e46bb7dc
Elham <elham.heydari722@gmail.com>
2025-02-24 18:04:00 +0330

    add registeration for persistence layer

---
+++ OmniMarket.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OmniMarket.Application.Persistence.Contracts;
+using OmniMarket.Persistence.Context;
+using OmniMarket.Persistence.Repositories;
+
+namespace OmniMarket.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OmniMarketDbContext>(options =>
+            {
+                options.UseSqlServer(configuration
+                    .GetConnectionString("OmniMarketConnectionString"));
+            });
+
+
+            #region Repository
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            #endregion
+
+            return services;
+        }
+
+    }
+}
