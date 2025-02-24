e5c88d800c7476f15d58c246dc7dc92b48f97585
Elham <elham.heydari722@gmail.com>
2025-02-24 17:49:28 +0330

    add product repository

---
+++ OmniMarket.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OmniMarket.Application.Persistence.Contracts;
+using OmniMarket.Domain.Entities;
+using OmniMarket.Persistence.Context;
+
+namespace OmniMarket.Persistence.Repositories
+{
+   public class ProductRepository :GenericRepository<Product>,IProductRepository
+   { 
+       private readonly OmniMarketDbContext _context;
+        public ProductRepository(OmniMarketDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
