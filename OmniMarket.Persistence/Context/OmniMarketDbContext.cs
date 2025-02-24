3f6ecbac8701d1184412e87ec2fd148e26dedd54
Elham <elham.heydari722@gmail.com>
2025-02-24 17:22:43 +0330

    some changes in context override onmodel creating and savechanges

--- OmniMarket.Persistence/Context/OmniMarketDbContext.cs
+++ OmniMarket.Persistence/Context/OmniMarketDbContext.cs
@@ -17,5 +17,37 @@ namespace OmniMarket.Persistence.Context
 
 
         public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(OmniMarketDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                entry.Entity.UpdateDate = DateTime.Now;
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreateDate = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                entry.Entity.UpdateDate=DateTime.Now;
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreateDate=DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
