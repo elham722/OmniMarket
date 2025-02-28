c1bf8afd9a501cc3d381da0e1ae77079a2e36c28
Elham <elham.heydari722@gmail.com>
2025-02-28 17:04:31 +0330

    add updatrs dtos for product

---
+++ OmniMarket.Application/DTOs/Product/UpdateProductDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OmniMarket.Application.DTOs.Product
+{
+   public class UpdateProductDto:BaseDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public int? Stock { get; set; }
+        public List<UpdateProductImageDto> ProductImages { get; set; } = new List<UpdateProductImageDto>();
+    }
+}
---
+++ OmniMarket.Application/DTOs/ProductImage/UpdateProductImageDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OmniMarket.Application.DTOs.ProductImage
+{
+    public class UpdateProductImageDto 
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+
+}
