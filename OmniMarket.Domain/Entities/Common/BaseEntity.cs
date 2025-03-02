b635e220e592e921da48f30d7f11081d5338eaea
Elham <elham.heydari722@gmail.com>
2025-03-02 07:04:38 +0330

    some changes in dbcontext

--- OmniMarket.Domain/Entities/Common/BaseEntity.cs
+++ OmniMarket.Domain/Entities/Common/BaseEntity.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
         public bool IsDeleted { get; set; } = false;
         public Guid CreatedById { get; set; }
         public Guid? UpdatedById { get; set; }
--- OmniMarket.Persistence/Context/OmniMarketDbContext.cs
+++ OmniMarket.Persistence/Context/OmniMarketDbContext.cs
@@ -1,25 +1,39 @@
-﻿
-namespace OmniMarket.Persistence.Context
+﻿namespace OmniMarket.Persistence.Context
 {
     public class OmniMarketDbContext(DbContextOptions<OmniMarketDbContext> options) : DbContext(options)
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(OmniMarketDbContext).Assembly);
         }
 
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken)
         {
             foreach (var entry in ChangeTracker.Entries<BaseEntity>())
             {
-                entry.Entity.UpdateDate = DateTime.Now;
                 if (entry.State == EntityState.Added)
                 {
-                    entry.Entity.CreateDate = DateTime.Now;
+                    entry.Entity.CreateDate = DateTime.UtcNow;
+                   
+                    if (entry.Entity.CreatedById == Guid.Empty)
+                    {
+                        entry.Entity.CreatedById = Guid.Parse("00000000-0000-0000-0000-000000000001");
+                    }
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdateDate = DateTime.UtcNow;
+                  
+                    entry.Entity.UpdatedById = Guid.Parse("00000000-0000-0000-0000-000000000001");
+                }
+                else if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.Entity.IsDeleted = true;
+                    entry.Entity.DeleteDate = DateTime.UtcNow;
                 }
             }
             return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
@@ -29,10 +43,24 @@ namespace OmniMarket.Persistence.Context
         {
             foreach (var entry in ChangeTracker.Entries<BaseEntity>())
             {
-                entry.Entity.UpdateDate=DateTime.Now;
                 if (entry.State == EntityState.Added)
                 {
-                    entry.Entity.CreateDate=DateTime.Now;
+                    entry.Entity.CreateDate = DateTime.UtcNow;
+                    if (entry.Entity.CreatedById == Guid.Empty)
+                    {
+                        entry.Entity.CreatedById = Guid.Parse("00000000-0000-0000-0000-000000000001");
+                    }
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdateDate = DateTime.UtcNow;
+                    entry.Entity.UpdatedById = Guid.Parse("00000000-0000-0000-0000-000000000001");
+                }
+                else if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.Entity.IsDeleted = true;
+                    entry.Entity.DeleteDate = DateTime.UtcNow;
                 }
             }
             return base.SaveChanges();
