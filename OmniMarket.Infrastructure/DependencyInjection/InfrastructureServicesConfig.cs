3b04a1e65cbeaccb1d3a981b803037b0c6461a0c
Elham <elham.heydari722@gmail.com>
2025-03-02 09:19:43 +0330

    complete infrastructure layer

---
+++ OmniMarket.Infrastructure/DependencyInjection/InfrastructureServicesConfig.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OmniMarket.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureServicesConfig
+    {
+        public static IServiceCollection InfrastructureServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
