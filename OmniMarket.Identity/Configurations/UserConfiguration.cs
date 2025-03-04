dc44ea0bcf127606c983e13e91607d0fd3081d1d
Elham <elham.heydari722@gmail.com>
2025-03-04 09:00:24 +0330

    add dbcontext identity

---
+++ OmniMarket.Identity/Configurations/UserConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmniMarket.Identity.Configurations
+{
+    public class UserConfiguration
+    {
+    }
+}
--- OmniMarket.Identity/Context/OmniMarketIdentityDbContext.cs
+++ OmniMarket.Identity/Context/OmniMarketIdentityDbContext.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OmniMarket.Identity.Configurations;
 using OmniMarket.Identity.Models;
 
 namespace OmniMarket.Identity.Context
 {
-  public class OmniMarketIdentityDbContext:IdentityDbContext<ApplicationUser>
+    public class OmniMarketIdentityDbContext(DbContextOptions<OmniMarketIdentityDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new RoleConfiguration());
+        }
     }
 }
