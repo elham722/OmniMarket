239a18bceee47689f41ca8cf82ddd98ef7f012ab
Elham <elham.heydari722@gmail.com>
2025-03-04 07:59:48 +0330

    add model for identity

---
+++ OmniMarket.Application/Models/Identity/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmniMarket.Application.Models.Identity
+{
+    public class AuthResponse
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ OmniMarket.Application/Models/Identity/RegistrationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmniMarket.Application.Models.Identity
+{
+    internal class RegistrationResponse
+    {
+        public string UserId { get; set; }
+    }
+}
