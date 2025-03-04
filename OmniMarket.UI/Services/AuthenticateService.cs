b71533e2edf8a49a66a8e3ad24db5898f528750d
Elham <elham.heydari722@gmail.com>
2025-03-04 15:55:24 +0330

    something

---
+++ OmniMarket.UI/Services/AuthenticateService.cs
@@ -0,0 +1,34 @@
+﻿
+using OmniMarket.UI.Contracts;
+using OmniMarket.UI.Services.Base;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace OmniMarket.UI.Services
+{
+    public class AuthenticateService : BaseHttpService, IAuthenticateService
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        private JwtSecurityTokenHandler _jwtSecurityTokenHandler;
+
+        public AuthenticateService(IClient client, ILocalStorageService localStorage, IHttpContextAccessor httpContextAccessor ) 
+            : base(client, localStorage)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        }
+        public Task<bool> Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Register(string firstName, string lastName, string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
