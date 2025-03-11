0c58fbd397c4ad9040edc1ed6d87392590c9f8ec
Elham <elham.heydari722@gmail.com>
2025-03-11 16:15:10 +0330

    add user dto

---
+++ OmniMarket.Application/DTOs/User/AssignRoleRequestDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OmniMarket.Application.DTOs.User
+{
+   public class AssignRoleRequestDto
+    {
+        public string UserId { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ OmniMarket.Application/DTOs/User/UserDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OmniMarket.Application.DTOs.User
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
